81548a06675bfbd92cc12e2ea1f26027f80b6e91
a-mason <187415356+a-mason@users.noreply.github.com>
2025-04-11 18:57:11 -0230

    Sessions added, minor changes of template

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.secret_key = "adam_project"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
+file_save_location = "static/images"
+allowed_types = [".png", ".jpg", ".jpeg"]
+collection_of_something = []
 @app.route("/")
-def welcome():
-    return render_template("welcome.html")
+def index():
+    if "collection_of_something" not in session:
+        session["collection_of_something"] = []
+    return render_template("index.html", collection_of_something=session["collection_of_something"], file_location=file_save_location)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

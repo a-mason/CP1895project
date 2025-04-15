e5aee2209dc4b77a24598c404b5ab0285d9dcb4f
a-mason <187415356+a-mason@users.noreply.github.com>
2025-04-15 14:47:43 -0230

    Changed collection, added battle option

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, request, redirect, flash
 from flask_session import Session
-import uuid, os
+import uuid, os, random
 
 app = Flask(__name__)
 app.secret_key = "adam_project"
@@ -10,12 +10,11 @@ Session(app)
 
 file_save_location = "static/images"
 allowed_types = [".png", ".jpg", ".jpeg"]
-collection_of_something = []
 @app.route("/")
 def index():
-    if "collection_of_something" not in session:
-        session["collection_of_something"] = []
-    return render_template("index.html", collection_of_something=session["collection_of_something"], file_location=file_save_location)
+    if "characters" not in session:
+        session["characters"] = []
+    return render_template("index.html", characters=session["characters"], file_location=file_save_location)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
@@ -23,9 +22,9 @@ def add():
         return render_template("add.html")
 
     elif request.method == "POST":
-        if "collection_of_something" not in session:
+        if "characters" not in session:
             print("Clearing session data")
-            session["collection_of_something"] = []
+            session["characters"] = []
 
         name = request.form.get("name", "invalid")
         info = request.form.get("info", "invalid")
@@ -38,7 +37,11 @@ def add():
                 unique_name = f"{uuid.uuid4().hex}{extension}"
                 filename = os.path.join(file_save_location, unique_name)
                 uploaded_file.save(filename)
-                session["collection_of_something"].append({"name": name, "info": info, "image": unique_name})
+                session["characters"].append({
+                    "name": name,
+                    "info": info,
+                    "image": unique_name,
+                    "hp": 10})
                 session.modified = True
                 flash("Added to collection", "message")
                 return redirect("/")
@@ -46,6 +49,17 @@ def add():
                 flash("Invalid file type, only .png, .jpg, .jpeg files allowed.", "error")
                 return redirect("/add")
 
+@app.route("/battle")
+def battle():
+    if "boss_hp" not in session:
+        session["boss_hp"] = 100
+    if "turn_taken" not in session:
+        session["turn_taken"] = []
+
+    return render_template("battle.html",
+                           characters=session["characters"],
+                           file_location=file_save_location,
+                           boss_hp=session["boss_hp"])
 
 if __name__ == "__main__":
     app.run(debug=True)
